943287fecd5f4347f96693076c48788153d5ea43
Diana K <dkriese@gmail.com>
2016-04-14 22:04:33 -0600

    added more fields and changed header size

--- public_html/player_stats.php
+++ public_html/player_stats.php
@@ -1,37 +1,42 @@
 <?php
-
-//$db_name = "test_gkstats";
-$db_name = "db_12465";
+require_once('config/config.php');
 
 if (isset($_POST["username"]) && !empty($_POST["username"])) { //Checks if action value exists
-	error_log(print_r($_POST, true));
 	$username = $_POST["username"];
-//		$con = mysql_connect("localhost", "root", "");
-	//$con = @mysql_connect("204.145.72.119", "db_12465", "7c3a6fb730");
-	$dsn = 'mysql:host=204.145.72.119;dbname=' . $db_name;
-	$uname = 'db_12465';
-	$pword = '7c3a6fb730';
-//	$options = array(
-//		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-//	);
 
 	$dbh = new PDO($dsn, $uname, $pword);
-	//$result = mysql_query("select * from player_stats where username = '" . $username . "'");
-try {
-   $stmt = $dbh->query("select * from player_stats where username = '" . $username . "'");
-	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch(PDOException $ex) {
-   error_log('Exception: ' . print_r($ex,true));
-}	
-	//error_log('results is ' . print_r($results, true));
+	try {
+		$sql = "SELECT time_ingame, points_captured, highest_killstreak, kills, deaths,"
+				. " cast(kills/deaths as decimal(10,2)) AS 'K/D', games_played, games_won as wins,"
+				. " games_played - games_won as losses, cast(games_won/(games_played - games_won) as decimal(10,2)) AS 'W/L',  "
+				. " red_team_count as red, blue_team_count as blue, "
+				. " cast(red_team_count/blue_team_count as decimal(10,2)) AS 'R/B' "
+				. " FROM player_stats WHERE username = :username";
+
+		$stmt = $dbh->prepare($sql);
+		$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+		$stmt->execute();
+		$results1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	} catch(PDOException $ex) {
+	   error_log('Exception: ' . print_r($ex,true));
+	}
+
+//Player Info Table:
+//Columns; id, uuid, lastname, rank, credits
+//Query: SELECT * FROM player_info WHERE lastname=’name_given’
+	try {
+		$stmt = $dbh->query("select lastname as name, rank, credits from player_info where lastname = '" . $username . "'");
+		$results2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	} catch(PDOException $ex) {
+	   error_log('Exception: ' . print_r($ex,true));
+	}
+	$results = array_merge($results2, $results1);
+
 	$data = array();
 	foreach ($results as $row) {
-		//error_log('row is ' . print_r($row, true));
 		$data[] = $row;
 	}
 }
-//}
-//$data = array('key' => 'value');
 echo json_encode($data);
 
 //username
